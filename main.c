78a30da990986b3618960a379c5470fb82d379f5
David Vasquez <83887793+FoulTrip@users.noreply.github.com>
2023-08-23 09:40:16 -0500

    act001

--- main.c
+++ main.c
@@ -1,32 +1,48 @@
 #include "executeCommand.h"
 
-#define MAX_INPUT_LENGTH 256
-
 /**
  * main - Creates an interactive loop in which the 
  *        user can enter commands and receive responses 
  *        from the program.
  * Return: Always 0.
  */
+
 int main()
 {
-	char input[MAX_INPUT_LENGTH];
+	ssize_t read;
+	char *input = NULL;
+	size_t len = 0;
+	int i;
 
 	while (1)
 	{
-		printf("$ ");
-		fflush(stdout);
-
-		if (fgets(input, sizeof(input), stdin) == NULL)
+		if (isatty(fileno(stdin)))
 		{
-			break;
+			printf("#cisfun$ ");
+		}
+
+		read = getline(&input, &len, stdin);
+
+		if (read == -1) 
+		{
+			free(input);
+			exit(0);
 		}
 
 		input[strcspn(input, "\n")] = '\0';
 
+		for(i = 0; input[i] != '\0'; i++)
+		{
+			if(input[i] != ' ')
+				break;
+		}
+		if (input[i] == '\0')
+			continue;
+
 		if (strcmp(input, "exit") == 0)
 		{
-			break;
+			free(input);
+			exit(0);
 		}
 		else if (strcspn(input, "env") == 0)
 		{
@@ -37,6 +53,6 @@ int main()
 			executeCommand(input);
 		}
 	}
-
+	free(input);
 	return (0);
 }
