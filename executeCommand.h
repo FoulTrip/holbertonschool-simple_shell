1d5dcbb065dde1e53a799ca1c690e6988638db82
David Vasquez <83887793+FoulTrip@users.noreply.github.com>
2023-08-23 14:15:21 -0500

    act003

--- executeCommand.h
+++ executeCommand.h
@@ -7,6 +7,8 @@
 #include <sys/wait.h>
 #include <stdbool.h>
 
+extern char **environ;
+
 void executeCommand(char *command);
 void printEnvironment();
 bool findExecutable(char *command, char *fullPath);
