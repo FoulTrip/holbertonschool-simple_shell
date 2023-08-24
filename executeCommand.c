c094147abf684c286b466489a3a88d29d3dc93b8
David Vasquez <83887793+FoulTrip@users.noreply.github.com>
2023-08-24 09:02:14 -0500

    act004

--- executeCommand.c
+++ executeCommand.c
@@ -1,29 +1,29 @@
-#include "executeCommand.h"
+#include "main.h"
 
 /**
  * executeCommand - It is responsible for executing a
  *                  command on a Unix or Linux system.
  * @command: string of input
  */
+
 void executeCommand(char *command)
 {
-	const char *delimiter = " ";
 	char *token;
 	pid_t child_pid;
 	int argIndex = 0;
 	char *args[20];
-	args[0] = NULL;
+	int status;
+	char fullPath[20];
 
-	for (token = strtok(command, delimiter); token != NULL; token = strtok(NULL, delimiter))
+	args[0] = NULL;
+	for (token = strtok(command, " "); token != NULL;
+			token = strtok(NULL, " "))
 	{
 		args[argIndex] = token;
 		argIndex++;
 	}
-
 	args[argIndex] = NULL;
-
 	child_pid = fork();
-
 	if (child_pid == -1)
 	{
 		perror("Error when creating a child process");
@@ -34,37 +34,17 @@ void executeCommand(char *command)
 	{
 		if (args[0] && !strchr(args[0], '/'))
 		{
-			char fullPath[20];
 			if (findExecutable(args[0], fullPath))
-			{
 				args[0] = fullPath;
-			}
 		}
-		execve(args[0], args, environ);
-		if (args[0] == NULL)
-			exit(2);
-		perror("./shell");
-		exit(EXIT_FAILURE);
+		if (execve(args[0], args, environ) == -1)
+		{
+			perror("./shell");
+			exit(EXIT_FAILURE);
+		}
 	}
 	else
 	{
-		int status;
 		wait(&status);
 	}
-
-}
-
-/**
- * printEnvironment - Prints the system environment variables.
- */
-void printEnvironment()
-{
-    extern char **environ;
-    char **env = environ;
-
-    while (*env != NULL)
-    {
-        printf("%s\n", *env);
-        env++;
-    }
 }
